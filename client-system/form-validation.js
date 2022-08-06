4b2fcf43479f949a4cfdac80465de790a90c2a7a
Andrei Golubenko <95508920+agolubenk@users.noreply.github.com>
2022-08-07 00:06:34 +0300

    +

---
+++ client-system/form-validation.js
@@ -0,0 +1,9 @@
+function getClient(){
+    var tat = '1234';
+    var pas = '1234';
+    a = document.getElementById('login');
+    b = document.getElementById('password');
+    if (a == tat){
+        alert(a)
+    }
+}
