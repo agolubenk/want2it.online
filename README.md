5f53648c220627ff32e999e7ed4a7ae16b4deffa
Andrei Golubenko <95508920+agolubenk@users.noreply.github.com>
2022-08-21 00:43:25 +0300

    +

--- README.md
+++
@@ -1,2 +0,0 @@
-# want2it.online
- 
--- client-system/form-validation.js
+++
@@ -1,9 +0,0 @@
-function getClient(){
-    var tat = '1234';
-    var pas = '1234';
-    a = document.getElementById('login');
-    b = document.getElementById('password');
-    if (a == tat){
-        alert(a)
-    }
-}
